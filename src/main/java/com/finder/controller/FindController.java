65fd3252d9f9772f727ff142b23b72e11977a878
Adam <adam.masnik@gmail.com>
2016-05-21 15:27:26 +0200

    basic ui

--- src/main/java/com/finder/controller/FindController.java
+++ src/main/java/com/finder/controller/FindController.java
@@ -1,4 +1,4 @@
-package com.finder.com.finder;
+package com.finder.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
